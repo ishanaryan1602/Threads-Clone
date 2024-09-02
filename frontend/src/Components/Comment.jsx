730805080e2f98ba4e2bb805db154616951d8791
ishanaryan1602 <github1602@gmail.com>
2024-09-02 12:36:36 +0530

    front end posts

---
+++ frontend/src/Components/Comment.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import {
+  Avatar,
+  Box,
+  Button,
+  Divider,
+  Flex,
+  Image,
+  Text,
+} from "@chakra-ui/react";
+import Actions from "./Actions";
+import { BsThreeDots } from "react-icons/bs";
+
+export default function Comment({
+  comment,
+  createdAt,
+  likes,
+  username,
+  userAvatar,
+}) {
+  const [liked, setLiked] = useState(false);
+
+  return (
+    <>
+      <Flex gap={4} py={2} my={2} w={"full"}>
+        <Avatar src={userAvatar} size={"sm"} />
+        <Flex gap={1} w={"full"} flexDirection={"column"}>
+          <Flex
+            w={"full"}
+            justifyContent={"space-between"}
+            alignItems={"center"}
+          >
+            <Text fontSize="sm" fontWeight="bold">
+              {username}
+            </Text>
+            <Flex gap={2} alignItems={"center"}>
+              <Text fontSize={"sm"} color={"gray.light"} > {createdAt} </Text>
+              <BsThreeDots />
+            </Flex>
+          </Flex>
+          <Text>{comment}</Text>
+          <Actions liked={liked} setLiked={setLiked} />
+          <Text fontSize={"sm"} color={"gray.light"}>
+            {likes + (liked ? 1 : 0)} likes
+          </Text>
+        </Flex>
+      </Flex>
+      <Divider my={4} borderColor={"gray.light"} />
+    </>
+  );
+}
--- frontend/src/pages/PostPage.jsx
+++ frontend/src/pages/PostPage.jsx
@@ -1,7 +1,72 @@
-import React from 'react'
+import {
+  Avatar,
+  Box,
+  Button,
+  Divider,
+  Flex,
+  Image,
+  Text,
+} from "@chakra-ui/react";
+import React, { useState } from "react";
+import { BsThreeDots } from "react-icons/bs";
+import Actions from "../Components/Actions";
+import Comment from "../Components/Comment";
 
 export default function PostPage() {
+  const [liked, setLiked] = useState(false);
   return (
-    <div>PostPage</div>
-  )
+    <>
+      <Flex>
+        <Flex w={"full"} alignItems={"center"} gap={3}>
+          <Avatar src="/zuck-avatar.png" size={"md"} name="Mark Zuckerberg" />
+          <Flex>
+            <Text fontSize={"sm"} fontWeight={"bold"}>
+              markzuckerberg
+            </Text>
+            <Image src="/verified.png" w="4" h={4} ml={4} />
+          </Flex>
+        </Flex>
+        <Flex gap={4} alignItems={"center"}>
+          <Text
+            fontSize={"xs"}
+            width={36}
+            textAlign={"right"}
+            color={"gray.light"}
+          >
+            1d ago
+          </Text>
+          <BsThreeDots />
+        </Flex>
+      </Flex>
+      <Text my={3}>Let's talk about threads</Text>
+      <Box borderRadius={6} overflow={"hidden"}>
+        <Image src="/post1.png" w={"full"} />
+      </Box>
+      <Flex gap={3} my={3}>
+        <Actions liked={liked} setLiked={setLiked} />
+      </Flex>
+      <Flex gap={2} alignItems={"center"}>
+        <Text color={"gray.light"} fontSize={"sm"}>
+          23 replies
+        </Text>
+        <Box w={0.5} h={0.5} borderRadius={"full"} bg={"gray.light"}></Box>
+        <Text color={"gray.light"} fontSize={"sm"}>
+          {200 + (liked ? 1 : 0)} likes
+        </Text>
+      </Flex>
+      {/* <Divider my={6} borderColor={"gray.light"} /> */}
+      <Flex justifyContent={"space-between"} my={8}>
+        <Flex gap={2} alignItems={"center"}>
+          <Text fontSize={"2xl"}>👋</Text>
+          <Text color={"gray.light"}>Get the app to like, reply and post.</Text>
+        </Flex>
+        <Button>Get</Button>
+      </Flex>
+      <Divider my={6} borderColor={"gray.light"} />
+      {/* <Box mb={"40px"}></Box> */}
+      <Comment comment="Looks reaally good!" createdAt="1d" likes={100} username="johndoe" userAvatar="https://bit.ly/dan-abramov" />
+      <Comment comment="Just amazing!" createdAt="2d" likes={50} username="mike" userAvatar="https://bit.ly/tioluwani-kolawole" />
+      <Comment comment="Really awesome!" createdAt="2d" likes={21} username="felecia" userAvatar="https://bit.ly/code-beast" />
+    </>
+  );
 }
